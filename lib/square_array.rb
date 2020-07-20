41927ad31925a9a99b53a2868c1234a429ff7596
Bella Evalle <bellaevalle@yahoo.com>
2020-07-20 18:59:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
 def square_array (array)
   final_result = []
-  count = 0
-  while count < array.length do
-    final_result.push(array[count]*array[count])
-    count += 1]
-  end
-  final_result
+count = 0 
+while count < array.length do 
+ final_result.push(array[count] * array[count])
+ count += 1
 end
+ final_result
+end
+
